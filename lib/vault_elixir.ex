03b18ab56e38e114d1ebd8653d8184a0902b741a
Erik Daughtrey <ed151@duke.edu>
2023-05-10 07:01:47 -0400

    initial elixir lib creation

---
+++ lib/vault_elixir.ex
@@ -0,0 +1,18 @@
+defmodule VaultElixir do
+  @moduledoc """
+  Documentation for `VaultElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VaultElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vault_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule VaultElixirTest do
+  use ExUnit.Case
+  doctest VaultElixir
+
+  test "greets the world" do
+    assert VaultElixir.hello() == :world
+  end
+end
